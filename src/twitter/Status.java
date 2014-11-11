ff99df3acc5845656c4bebd9bef6ca65b807ccf5
Varlamova <silver.varlam@mail.ru>
2014-11-11 22:50:01 +0200

    Added fields of class Status

---
+++ src/twitter/Status.java
@@ -0,0 +1,18 @@
+package twitter;
+
+public class Status {
+int ID;
+String text;
+int UserID;
+String UserName;
+String Date;
+//Date and time of status creation
+Boolean Media;
+//Presence or absence of picture, video or audio
+String Hashtag;
+int FavoriteCount;
+String Favorited;
+int RetweetCount;
+String Retvited;
+String Language;
+}

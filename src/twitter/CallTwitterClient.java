2fa45f50c2d7ef2dd60427886fa68dc6e99d6260
Varlamova <silver.varlam@mail.ru>
2014-11-17 22:35:38 +0200

    Added CallTwitterClientTest.

--- src/twitter/CallTwitterClient.java
+++ src/twitter/CallTwitterClient.java
@@ -1,55 +1,65 @@
 package twitter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class CallTwitterClient  {
-	
+		
 	public static void main (String args[]) {
 		StringBuffer result = new StringBuffer();
 		for (int i = 0; i < args.length; i++) {
 		   result.append(args[i]);
 		   	}
 		
-		String enter = result.toString();
-		if (enter.indexOf("getStatus")>0) {
+		//String enter = result.toString();
+		String enter = ("--cmd=getStatus--id=57687678");
+		extractCommands(enter);
+	}
+	public static  Set<String> extractCommands(String enter) {
+		Set<String> commands = new HashSet<String>();
+			if (enter.indexOf("getStatus")>0) {
 			String cmd = "getStatus";
+			commands.add(cmd);
 			int start = enter.indexOf("id")+3;
 			// 3 is a magic number that means number of characters in phrase id=
 			int end = enter.length();
 			char buf[] = new char[end - start];
 			enter.getChars(start, end, buf, 0);
 			String id = new String(buf);
-			System.out.println("cmd=" + cmd);
-			System.out.println("id=" + id);
-			}
+			commands.add(id);
+				}
 			else  if (enter.indexOf("postStatus")>0) {
 				String cmd = "postStatus";
+				commands.add(cmd);
 				int start = enter.indexOf("text")+5;
 				// 5 is a magic number that means number of characters in phrase text=
 				int end = enter.length();
 				char buf[] = new char[end - start];
 				enter.getChars(start, end, buf, 0);
 				String text = new String(buf);
-				System.out.println("cmd=" + cmd);
-				System.out.println("text=" + text);
+				commands.add(text);
 					}
 				else if (enter.indexOf("getUserTimeline")>0) {
 					String cmd = "getUserTimeline";
+					commands.add(cmd);
 					int start = enter.indexOf("id")+3;
 					//  3 is a magic number that means number of characters in phrase id=
 					int end = enter.indexOf("--limit");
 					char buf[] = new char[end - start];
 					enter.getChars(start, end, buf, 0);
 					String id = new String(buf);
+					commands.add(id);
 					int start2 = enter.indexOf("limit")+6;
 					//  6 is a magic number that means number of characters in phrase limit=
 					int end2 = enter.length();
 					char buf2[] = new char[end2 - start2];
 					enter.getChars(start2, end2, buf2, 0);
 					String limit = new String(buf2);
-					System.out.println("cmd=" + cmd);
-					System.out.println("text=" + id);
-					System.out.println("limit=" + limit);
+					commands.add(limit);
 						}
 						else { System.out.println("Usage: 1)--cmd=getStatus--id=, 2) --cmd=postStatus--text=,3)--cmd=getUserTimeLine--id=--limit=");}
+		
+				return commands;
 		
 			}
 
---
+++ src/twitter/CallTwitterClientTest.java
@@ -0,0 +1,25 @@
+package twitter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import junit.framework.TestCase;
+
+
+public class CallTwitterClientTest extends TestCase {
+	
+	public void TestCallTwitterClient () {
+		String test  = ("--cmd=getStatus--id=57687678");
+		
+		Set <String> actual = CallTwitterClient.extractCommands(test);
+		
+		Set<String> expected = new HashSet<String>();
+		expected.add("getStatus");
+		expected.add("57687678");
+		
+		assertEquals(actual, expected);
+				
+	}
+
+	
+}

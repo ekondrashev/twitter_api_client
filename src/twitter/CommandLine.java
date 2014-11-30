4552a3c7766d24c882e3021d2a74dae8c4e34395
Varlamova <silver.varlam@mail.ru>
2014-12-01 00:33:02 +0200

    I corrected my CommandLine again)))

--- src/twitter/CommandLine.java
+++ src/twitter/CommandLine.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class CallTwitterClient {
+public class CommandLine {
 
 	public static void main(String[] args) {
 		extractCommands(args);

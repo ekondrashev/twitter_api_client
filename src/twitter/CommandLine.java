090de630f502dfbd3bd49d750710c3d8b888b088
Varlamova <silver.varlam@mail.ru>
2014-12-03 00:21:22 +0200

    New version of command line.

--- src/twitter/CommandLine.java
+++ src/twitter/CommandLine.java
@@ -1,7 +1,9 @@
 package twitter;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,65 +13,75 @@ public class CommandLine {
 		extractCommands(args);
 	}
 	
-	public static  Map <String, String> extractCommands (String[] args) {
+	public static  void extractCommands (String[] args) {
 		
 		Map <String, String> commands = new HashMap<String, String>();
 		
-		String usage = "Usage: 1)--cmd=\"getStatus\"--id=\"¬ведите id сообщени€\","
-					+ "2) --cmd=\"postStatus\"--text=\"¬ведите текст сообщени€\","
-					+ "3)--cmd=\"getUserTimeLine\"--id=\"¬ведите id пользовател€\"--limit=\"¬ведите количество необходимых сообщений\"";
+		for (String arg: args) {
+			if (Pattern.compile("getstatus|get_status").matcher(arg.toLowerCase()).find()) {
+				commands.put("cmd", "getStatus");
+			} else if (Pattern.compile("poststatus|post_status").matcher(arg.toLowerCase()).find()) {
+				commands.put("cmd", "postStatus");
+			} else if (Pattern.compile("getusertimeline|get_usertimeline|get_user_timeline").matcher(arg.toLowerCase()).find()){
+				commands.put("cmd", "getUserTimeline");
+			}
+		}
 		
-		StringBuffer result = new StringBuffer();
-		for (int i = 0; i < args.length; i++) {
-		   result.append(args[i]);
-		   	}
-		String input = result.toString();
-					
-		if (Pattern.compile("(?=.*--cmd=(getstatus|get_status))(?=.*--id=(\\d+)).*").matcher(input.toLowerCase()).matches()) {
-				
+		
+		for (String arg: args) {
+			if (Pattern.compile("--id=(\\d+)").matcher(arg.toLowerCase()).find()) {
 				Pattern pattern = Pattern.compile("id=(\\d+)");
-				Matcher matcher = pattern.matcher(input.toLowerCase());
+				Matcher matcher = pattern.matcher(arg.toLowerCase());
 				matcher.find();
 				String id = matcher.group(1);
-				commands.put("cmd", "getStatus");
 				commands.put("id",id);
-						
-				} else if (Pattern.compile("(?=.*--cmd=(poststatus|post_status))(?=.*--text=(.{1,140})).*").matcher(input.toLowerCase()).matches()) {
-					
-					commands.put("cmd", "postStatus");
-					
-						for (String arg: args) {
-							if(Pattern.compile("--text=(.{1,140})").matcher(arg.toLowerCase()).matches()) {
-								Pattern pattern = Pattern.compile("=(.{1,140})");
-								Matcher matcher = pattern.matcher(arg);
-								System.out.println(matcher.find());
-								System.out.println(matcher.group(1));
-								String text = matcher.group(1);
-								commands.put("text", text);
-								}
-						}
+			} else if (Pattern.compile("--text=(.{1,140})").matcher(arg.toLowerCase()).find()) {
+				Pattern pattern = Pattern.compile("--text=(.{1,140})");
+				Matcher matcher = pattern.matcher(arg.toLowerCase());
+				matcher.find();
+				String text = matcher.group(1);
+				commands.put("text", text);
+			} else if (Pattern.compile("--limit=(\\d{1,3})").matcher(arg.toLowerCase()).find()) {
+				Pattern pattern = Pattern.compile("--limit=(\\d{1,3})");
+				Matcher matcher = pattern.matcher(arg.toLowerCase());
+				matcher.find();
+				String limit = matcher.group(1);
+				commands.put("limit", limit);
+			}
+		}
+				
+			Set <String> keysGetStatus = new HashSet <String>();
+			keysGetStatus.add("cmd");
+			keysGetStatus.add("id");
 			
-					} else if (Pattern.compile("(?=.*--cmd=(getusertimeline|get_usertimeline))(?=.*--id=(\\d+))(?=.*--limit=(\\d{1,3}))?.*").matcher(input.toLowerCase()).matches()) {
-				
-						commands.put("cmd", "getUserTimeline");
-						Pattern pattern = Pattern.compile("id=(\\d+)");
-						Matcher matcher = pattern.matcher(input.toLowerCase());
-						matcher.find();
-						String id = matcher.group(1);
-						commands.put("id",id);
-				
-							if (Pattern.compile("limit").matcher(input.toLowerCase()).find()) {
-					
-								Pattern patternL = Pattern.compile("limit=(\\d{1,3})");
-								Matcher matcherL = patternL.matcher(input.toLowerCase());
-								matcherL.find();
-								String limit = matcherL.group(1);
-								commands.put("limit", limit);
-							}
-					} 	else {System.out.println(usage);}
-		
-				
-		return commands;
+			Set <String> keysPostStatus = new HashSet <String>();
+			keysPostStatus.add("cmd");
+			keysPostStatus.add("text");
 			
+			Set <String> keysGetUserTL = new HashSet <String>();
+			keysGetUserTL.add("cmd");
+			keysGetUserTL.add("id");
+			keysGetUserTL.add("text");
+			
+			String usage = "Usage: 1)--cmd=\"getStatus\"--id=\"Enter message ID\","
+					+ "2) --cmd=\"postStatus\"--text=\"Enter the message (up to 140 characters)\","
+					+ "3)--cmd=\"getUserTimeLine\"--id=\"Enter user ID\"--limit=\"Enter the number of messages (up to 100 messages)\"";
+			
+			if (commands.keySet().equals(keysGetStatus) && commands.get("cmd").equals("getStatus")) {
+				returnCommands(commands);				
+			} else if (commands.keySet().equals(keysPostStatus) && commands.get("cmd").equals("postStatus")) {
+				returnCommands(commands);
+			} else if (commands.keySet().equals(keysGetUserTL) && commands.get("cmd").equals("getUserTimeline")) {
+				returnCommands(commands);
+			} else { System.out.println(usage);}
+			
+				
 	}
+	
+	public static  Map <String, String> returnCommands (Map <String, String> commands) {
+		
+		return commands;
+		
+	}
+	
 }

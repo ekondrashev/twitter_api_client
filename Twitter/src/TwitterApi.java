c995a8e23ac48d15b3d08a7621ccbf8802b351cd
Olinichenko <olin.kot@gmail.com>
2014-12-10 16:53:51 +0200

    1

--- Twitter/src/TwitterApi.java
+++ Twitter/src/TwitterApi.java
@@ -1,20 +1,48 @@
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 public class TwitterApi {
+	
 	private static String consumerKey="mb5yNKsX0OVr1GRmhfDfcGBgX";
 	private static String consumerSecret ="2dQ0Wsbz8x03v8HwTmPt8CMeEPg9rMe3Oxws9GHrSqUVyO2jXj";
 	private static String accessToken ="2909023707-FAIlWfTw2v8ziqZxP46AMSsq1dqSY29Boo11qCt";
 	private static String accessTokenSecret ="qLxbRDgnLDTvnSx41Y8MWc9x0P80EjmFx21JQiw6IZhvt";
 
 	private static String urlAuth="https://api.twitter.com/oauth2/token";
-	private static String owner ="a__kot";
-	private static String ownerID ="2909023707";
+	//private static String owner ="a__kot";
+	//private static String ownerID ="2909023707";
 	
-	private static String signatureBaseString ="GET&https%3A%2F%2Fapi.twitter.com%2F1.1%2F&oauth_consumer_key%3Dmb5yNKsX0OVr1GRmhfDfcGBgX%26oauth_nonce%3Dd57233d49e1590a471ea9d7403371eb4%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1418161737%26oauth_token%3D2909023707-FAIlWfTw2v8ziqZxP46AMSsq1dqSY29Boo11qCt%26oauth_version%3D1.0";
-	//private static String authorizationHeader ="Authorization: OAuth oauth_consumer_key=""mb5yNKsX0OVr1GRmhfDfcGBgX"", oauth_nonce=""d57233d49e1590a471ea9d7403371eb4"", oauth_signature=""0FDw4AFYuSavzvAx%2BXjL7rjJQgs%3D"", oauth_signature_method=""HMAC-SHA1"", oauth_timestamp=""1418161737"", oauth_token=""2909023707-FAIlWfTw2v8ziqZxP46AMSsq1dqSY29Boo11qCt"", oauth_version=""1.0""";
-	//private static String cURLCommand = "url --get 'https://api.twitter.com/1.1/' --header 'Authorization: OAuth oauth_consumer_key=""mb5yNKsX0OVr1GRmhfDfcGBgX"", oauth_nonce=""d57233d49e1590a471ea9d7403371eb4"", oauth_signature=""0FDw4AFYuSavzvAx%2BXjL7rjJQgs%3D"", oauth_signature_method=""HMAC-SHA1"", oauth_timestamp=""1418161737"", oauth_token=""2909023707-FAIlWfTw2v8ziqZxP46AMSsq1dqSY29Boo11qCt"", oauth_version=""1.0""' --verbose";
+	//private static String signatureBaseString ="GET&https%3A%2F%2Fapi.twitter.com%2F1.1%2F&oauth_consumer_key%3Dmb5yNKsX0OVr1GRmhfDfcGBgX%26oauth_nonce%3Dd57233d49e1590a471ea9d7403371eb4%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1418161737%26oauth_token%3D2909023707-FAIlWfTw2v8ziqZxP46AMSsq1dqSY29Boo11qCt%26oauth_version%3D1.0";
+	private static String authorizationHeader ="Authorization: OAuth oauth_consumer_key=\"mb5yNKsX0OVr1GRmhfDfcGBgX\", oauth_nonce=\"d57233d49e1590a471ea9d7403371eb4\", oauth_signature=\"0FDw4AFYuSavzvAx%2BXjL7rjJQgs%3D\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"1418161737\", oauth_token=\"2909023707-FAIlWfTw2v8ziqZxP46AMSsq1dqSY29Boo11qCt\", oauth_version=\"1.0\"";
+	//private static String cURLCommand = "url --get 'https://api.twitter.com/1.1/' --header 'Authorization: OAuth oauth_consumer_key=\"mb5yNKsX0OVr1GRmhfDfcGBgX\", oauth_nonce=\"d57233d49e1590a471ea9d7403371eb4\", oauth_signature=\"0FDw4AFYuSavzvAx%2BXjL7rjJQgs%3D\", oauth_signature_method=\"HMAC-SHA1\", oauth_timestamp=\"1418161737\", oauth_token=\"2909023707-FAIlWfTw2v8ziqZxP46AMSsq1dqSY29Boo11qCt\", oauth_version=\"1.0\"' --verbose";
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	    HttpClient httpClient = new DefaultHttpClient();
+	    try {
+// 	      HttpGet httpGetRequest = new HttpGet(urlAuth +" "+ authorizationHeader);
+//	      HttpResponse httpResponse = httpClient.execute(httpGetRequest);
+	      
+	      HttpPost httpPostRequest = new HttpPost(urlAuth +" "+ authorizationHeader);
+	      HttpResponse httpResponse = httpClient.execute(httpPostRequest);
+	 
+	      System.out.println("----------------------------------------");
+	      System.out.println(httpResponse.getStatusLine());
+	      System.out.println("----------------------------------------");
+	 
+	      // Get hold of the response entity
+	      HttpEntity entity = httpResponse.getEntity();
+	    } catch (IOException e) {
+		      // thrown by entity.getContent();
+		      e.printStackTrace();
+		    } finally {
+		      httpClient.getConnectionManager().shutdown();
+		    }
 	}
 
 }

121947da50ffff13d8eaa0631e98e115c50b86be
Katechka <Katechka@192.168.0.100>
2014-12-24 20:20:52 +0200

    New new interface!

--- src/twitter/TwitterClient.java
+++ src/twitter/TwitterClient.java
@@ -7,6 +7,6 @@ import java.util.List;
 public interface TwitterClient {
 	public Status getStatus(long statusId);
 	public long postStatus(String text);
-	public List <String> getUserTimeline(String userName, int limit);
+	public List <Status> getUserTimeline(String userName, int limit);
 
 }

390e30e77006ee423fbd1bdb565ad5dec224303e
Katechka <silver.varlam@mail.ru>
2015-01-30 21:21:41 +0200

    Added CountDownLatch.

--- src/twitter/CounterMult.java
+++ src/twitter/CounterMult.java
@@ -1,66 +1,50 @@
 package twitter;
 
+import java.util.concurrent.CountDownLatch;
+
 public class CounterMult {
-	
+
 	int counter = 0;
-	CounterRun countRun = new CounterRun();
-	
+	CountDownLatch latch = new CountDownLatch(10);
+	CounterRun countRun = new CounterRun(latch);
+
 	public synchronized void increment() {
 		counter++;
-	 }
-     
-	 public int execute(){
-		 
-		 Thread thread1 = new Thread (countRun);
-		 Thread thread2 = new Thread (countRun);
-		 Thread thread3 = new Thread (countRun);
-		 Thread thread4 = new Thread (countRun);
-		 Thread thread5 = new Thread (countRun);
-		 Thread thread6 = new Thread (countRun);
-		 Thread thread7 = new Thread (countRun);
-		 Thread thread8 = new Thread (countRun);
-		 Thread thread9 = new Thread (countRun);
-		 Thread thread10 = new Thread (countRun);
-		 
-		 thread1.start();
-		 thread2.start();
-		 thread3.start();
-		 thread4.start();
-		 thread5.start();
-		 thread6.start();
-		 thread7.start();
-		 thread8.start();
-		 thread9.start();
-		 thread10.start();
-		 
-		 
-		 try {			 
-			thread1.join();
-			thread2.join();
-			thread3.join();
-			thread4.join();
-			thread5.join();
-			thread6.join();
-			thread7.join();
-			thread8.join();
-			thread9.join();
-			thread10.join();
-						
-		 } catch (InterruptedException ignore) {/*NOP*/}
-		 
-		 return counter;
-		 
-	 }
-	 class CounterRun implements Runnable{
+	}
 
-			@Override
-			public void run() {
-				
-				for (int i = 0; i < 1000000; i++)
-                {
-                    increment();
-                }
-				
-			}       
-}
+	public int execute() {
+
+		for (int i = 0; i < 10; i++) {
+			new Thread(countRun).start();
+		}
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return counter;
+
+	}
+
+	class CounterRun implements Runnable {
+
+		CountDownLatch latch = null;
+
+		public CounterRun(CountDownLatch latch) {
+			this.latch = latch;
+		}
+
+		@Override
+		public void run() {
+
+			for (int i = 0; i < 1000000; i++) {
+				increment();
+			}
+
+			latch.countDown();
+
+		}
+	}
 }
--- src/twitter/CounterTest.java
+++ src/twitter/CounterTest.java
@@ -5,23 +5,23 @@ import org.junit.Test;
 import junit.framework.TestCase;
 
 public class CounterTest extends TestCase {
-	
-	 @Test
-	public void testCounter(){
+
+	@Test
+	public void testCounter() {
 		Counter counter = new Counter();
 		int actual = counter.execute();
 		int expected = 10000000;
-		assertEquals(actual,expected);
+		assertEquals(actual, expected);
 	}
-	
-	 @Test
-		public void testCounterMult(){
-		 for (int i= 0; i<100; i++){
-			 CounterMult counter = new CounterMult();
-			 int actual = counter.execute();
-			 int expected = 10000000;
-			 assertEquals(actual,expected);
-	 }
-	 }
+
+	@Test
+	public void testCounterMult() {
+		for (int i = 0; i < 100; i++) {
+			CounterMult counter = new CounterMult();
+			int actual = counter.execute();
+			int expected = 10000000;
+			assertEquals(actual, expected);
+		}
+	}
 
 }

f02a6096d85a4efc24c4a0c3f7d2704f28551a0e
Olinichenko <olin.kot@gmail.com>
2015-02-10 19:12:54 +0200

    filecopy

---
+++ Twitter/src/HW/CopyFile.java
@@ -0,0 +1,47 @@
+package HW;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class CopyFile {
+
+    public static void main(String[] args) throws IOException {
+
+
+        FileInputStream in = null;
+
+        FileOutputStream out = null;
+
+
+        try {
+
+            in = new FileInputStream("C:\\in.txt");
+
+            out = new FileOutputStream("C:\\out.txt");
+
+            int c;
+
+			byte[] buffer = new byte[1024];
+
+            while ((c = in.read(buffer)) != -1) {
+            	out.write(buffer, 0, c);
+ 
+
+            }
+
+        } finally {
+
+            if (in != null) {
+
+                in.close();
+
+            }
+
+            if (out != null) {
+
+                out.close();
+
+            }}}}
+

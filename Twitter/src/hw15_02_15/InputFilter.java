d2bd475411f8fa8f225611c1f7500c6bec9f34d7
Olinichenko <olin.kot@gmail.com>
2015-02-12 19:00:24 +0200

    in reseach

---
+++ Twitter/src/hw15_02_15/InputFilter.java
@@ -0,0 +1,19 @@
+package hw15_02_15;
+import java.io.FilterReader;
+import java.io.;
+
+public class InputFilter {
+
+	public static String a = "тестовое сообщение!" ;
+
+	public static void main(String[] args) {
+		
+		Readtext(a);
+	}
+
+	private static void Readtext(String str) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

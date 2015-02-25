3a72c84a747bc706afc05fa601ad9e2bf0935a15
Olinichenko <olin.kot@gmail.com>
2015-02-25 10:25:18 +0200

    hw 25-02-15

---
+++ Twitter/src/IOandNIO/ClientNIO.java
@@ -0,0 +1,56 @@
+package IOandNIO;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.util.concurrent.Future;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.util.concurrent.ExecutionException;
+import java.net.InetSocketAddress;
+
+public class ClientNIO {
+
+    public static void main (String [] args)
+            throws Exception {
+	
+        new ClientNIO().go();
+    }
+
+    private void go()
+            throws IOException, InterruptedException, ExecutionException {
+	
+        AsynchronousSocketChannel client = AsynchronousSocketChannel.open();
+        InetSocketAddress hostAddress = new InetSocketAddress("localhost", 8086);
+        Future<Void> future = client.connect(hostAddress);
+        future.get(); // returns null
+
+        System.out.println("Client is started: " + client.isOpen());
+        System.out.println("Sending messages to server: ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+			String in = br.readLine();
+			if (in.equalsIgnoreCase("exit")) {return;}
+            byte [] message = in.getBytes();
+            ByteBuffer buffer = ByteBuffer.wrap(message);
+            Future<Integer> result = client.write(buffer);
+		
+            while (! result.isDone()) {
+                System.out.println("Sending... ");
+            }
+            buffer.flip();
+            result = client.read(buffer); 
+            while (! result.isDone()) {
+                System.out.println("Waiting answer... ");
+            }
+         
+            System.out.println(new String(buffer.array()).trim()); 
+            buffer.clear();
+ //           Thread.sleep(3000);
+		}
+ 		
+	//	client.close();
+ 		
+	}
+}
---
+++ Twitter/src/IOandNIO/ServerNIO.java
@@ -0,0 +1,129 @@
+package IOandNIO;
+
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousChannelGroup;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+
+
+public class ServerNIO {
+	private static int port = 8086;
+	
+	public ServerNIO() {
+		
+		startServer(port);
+	}
+	
+	public static void main(String[] args) {
+		if (args.length>0 && args[0]!=null){
+			int tempPort = Integer.parseInt(args[0]);
+			if(tempPort >0) port=tempPort;
+		}
+		
+		startServer(port);
+
+	}
+	private static void startServer(int i) {
+		try {
+			AsynchronousChannelGroup group = AsynchronousChannelGroup.withThreadPool(Executors.newSingleThreadExecutor());
+			AsynchronousServerSocketChannel server =
+				    AsynchronousServerSocketChannel.open(group).bind(new InetSocketAddress(i));
+
+	           System.out.println("Server listening on " + port);
+
+	           server.accept("Attachment", new CompletionHandler<AsynchronousSocketChannel, String>() {
+	               @Override
+	               public void completed(AsynchronousSocketChannel result, String attachment) {
+	                   System.out.println("Completed");
+	                   server.accept(null, this);
+	             
+	                   handle(result);
+	                   try {
+	                       System.out.println("Accepted: " + result.getRemoteAddress());
+
+	                   } catch (Exception e) {
+	                       e.printStackTrace();
+	                   }
+	               }
+
+	               @Override
+	               public void failed(Throwable exc, String attachment) {
+	                   System.out.println("Failed");
+	               }
+	           });
+	            
+			group.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);			
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		} 
+		
+	}
+	
+    private static void handle(AsynchronousSocketChannel clientChannel) {
+       
+
+        System.out.println("Messages from client: ");
+
+        if ((clientChannel != null) && (clientChannel.isOpen())) {
+        
+            ByteBuffer buffer = ByteBuffer.allocate(100);
+            
+            Future<Integer> result = clientChannel.read(buffer);
+ //           while (true) {
+            int bytesRead = 0;
+			try {
+				bytesRead = result.get(10,TimeUnit.SECONDS);
+				System.out.println(bytesRead);
+			} catch (InterruptedException | ExecutionException | TimeoutException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} //read into buffer.
+            while (bytesRead != -1) {
+
+
+                while (! result.isDone()) {
+                    // do nothing
+                }
+                
+                String clientString = new String(buffer.array()).trim();
+                System.out.println(clientString); 
+                buffer.flip();
+                buffer = ByteBuffer.wrap(("Serv: "+clientString).getBytes());
+                result = clientChannel.write(buffer);
+ //               System.out.println(new String(buffer.array()).trim()); 
+                while (! result.isDone()) {
+                    // do nothing
+                }    
+                
+                buffer.clear(); //make buffer ready for writing
+                
+                try {
+					bytesRead = clientChannel.read(buffer).get();
+				} catch (InterruptedException | ExecutionException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+
+            } // while()
+
+
+        } // end-if
+  	
+    }
+
+	private static String String(byte[] array) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
